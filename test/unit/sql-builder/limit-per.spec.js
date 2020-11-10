8d2329e0bd760a41891f44fbcb2521c6cadd5a10
Marco Baumgartl <marco.baumgartl@boerse-go.de>
2020-11-10 15:18:10 +0100

    Implement limit-per filters

--- test/unit/sql-builder/limit-per.spec.js
+++ test/unit/sql-builder/limit-per.spec.js
@@ -49,15 +49,15 @@ describe('query-builder (limit-per)', () => {
             filter: [[{ attribute: 'col1', operator: 'equal', valueFromParentKey: true, value: [1, 3] }]]
         });
 
-        const [, laterlJoin] = ast.from;
-        const { expr: originalQuery } = laterlJoin;
+        const [, lateralJoin] = ast.from;
+        const { expr: originalQuery } = lateralJoin;
         expect(originalQuery).to.have.property('type', 'select');
         expect(originalQuery).to.have.property('from').and.to.eql(queryAst.from);
         expect(originalQuery).to.have.property('parentheses', true);
-        expect(laterlJoin).to.have.property('join', 'JOIN');
-        expect(laterlJoin).to.have.property('lateral', true);
-        expect(laterlJoin).to.have.property('on').and.to.eql({ type: 'boolean', value: true });
-        expect(laterlJoin).to.have.property('as', 'limitper');
+        expect(lateralJoin).to.have.property('join', 'JOIN');
+        expect(lateralJoin).to.have.property('lateral', true);
+        expect(lateralJoin).to.have.property('on').and.to.eql({ type: 'boolean', value: true });
+        expect(lateralJoin).to.have.property('as', 'limitper');
     });
 
     it('should select attributes from derived table alias', () => {
@@ -77,7 +77,7 @@ describe('query-builder (limit-per)', () => {
         ]);
     });
 
-    xdescribe('filters', () => {
+    describe('filters', () => {
         it('should generate WHERE clause for correlated subquery', () => {
             const ast = queryBuilder({
                 attributes: ['col2'],
@@ -132,7 +132,8 @@ describe('query-builder (limit-per)', () => {
                         type: 'binary_expr',
                         operator: '=',
                         left: { type: 'column_ref', table: 'limitper_ids', column: 'id' },
-                        right: { type: 'column_ref', table: 't', column: 'col1' }
+                        right: { type: 'column_ref', table: 't', column: 'col1' },
+                        parentheses: true
                     }
                 });
         });
@@ -185,29 +186,14 @@ describe('query-builder (limit-per)', () => {
                             left: {
                                 type: 'binary_expr',
                                 operator: '<=',
-                                left: {
-                                    column: 'col2',
-                                    table: 't',
-                                    type: 'column_ref'
-                                },
-                                right: {
-                                    type: 'number',
-                                    value: 10
-                                }
+                                left: { column: 'col2', table: 't', type: 'column_ref' },
+                                right: { type: 'number', value: 10 }
                             },
                             right: {
                                 type: 'binary_expr',
                                 operator: '=',
-                                left: {
-                                    column: 'id',
-                                    table: 'limitper_ids',
-                                    type: 'column_ref'
-                                },
-                                right: {
-                                    column: 'col1',
-                                    table: 't',
-                                    type: 'column_ref'
-                                }
+                                left: { column: 'id', table: 'limitper_ids', type: 'column_ref' },
+                                right: { column: 'col1', table: 't', type: 'column_ref' }
                             }
                         },
                         right: {
@@ -216,29 +202,14 @@ describe('query-builder (limit-per)', () => {
                             left: {
                                 type: 'binary_expr',
                                 operator: '>=',
-                                left: {
-                                    column: 'col2',
-                                    table: 't',
-                                    type: 'column_ref'
-                                },
-                                right: {
-                                    type: 'number',
-                                    value: 5
-                                }
+                                left: { column: 'col2', table: 't', type: 'column_ref' },
+                                right: { type: 'number', value: 5 }
                             },
                             right: {
                                 type: 'binary_expr',
                                 operator: '=',
-                                left: {
-                                    column: 'id',
-                                    table: 'limitper_ids',
-                                    type: 'column_ref'
-                                },
-                                right: {
-                                    column: 'col1',
-                                    table: 't',
-                                    type: 'column_ref'
-                                }
+                                left: { column: 'id', table: 'limitper_ids', type: 'column_ref' },
+                                right: { column: 'col1', table: 't', type: 'column_ref' }
                             }
                         },
                         parentheses: true
@@ -246,45 +217,78 @@ describe('query-builder (limit-per)', () => {
                 });
         });
 
-        xit('should support search?');
+        it('should support fulltext searches', () => {
+            const ast = queryBuilder({
+                attributes: ['col2'],
+                queryAst,
+                limitPer: 'col1',
+                limit: 5,
+                filter: [[{ attribute: 'col1', operator: 'equal', valueFromParentKey: true, value: [1, 3] }]],
+                search: 'te%st',
+                searchable: ['col2']
+            });
+
+            const [, { expr: subSelect }] = ast.from;
+            expect(subSelect)
+                .to.have.property('where')
+                .and.to.eql({
+                    type: 'binary_expr',
+                    operator: 'AND',
+                    left: {
+                        type: 'binary_expr',
+                        operator: '=',
+                        left: { type: 'column_ref', table: 'limitper_ids', column: 'id' },
+                        right: { type: 'column_ref', table: 't', column: 'col1' }
+                    },
+                    right: {
+                        type: 'binary_expr',
+                        operator: 'LIKE',
+                        left: { type: 'column_ref', table: 't', column: 'col2' },
+                        right: { type: 'string', value: '%te\\%st%' },
+                        parentheses: true
+                    }
+                });
+        });
     });
 
-    it('should apply limit to lateral join', () => {
-        const ast = queryBuilder({
-            attributes: ['col2'],
-            queryAst,
-            limitPer: 'col1',
-            limit: 5,
-            filter: [[{ attribute: 'col1', operator: 'equal', valueFromParentKey: true, value: [1, 3] }]]
+    describe('limit', () => {
+        it('should apply limit to lateral join', () => {
+            const ast = queryBuilder({
+                attributes: ['col2'],
+                queryAst,
+                limitPer: 'col1',
+                limit: 5,
+                filter: [[{ attribute: 'col1', operator: 'equal', valueFromParentKey: true, value: [1, 3] }]]
+            });
+
+            const [, { expr: subSelect }] = ast.from;
+            expect(subSelect)
+                .to.have.property('limit')
+                .and.to.eql([
+                    { type: 'number', value: 0 },
+                    { type: 'number', value: 5 }
+                ]);
         });
 
-        const [, { expr: subSelect }] = ast.from;
-        expect(subSelect)
-            .to.have.property('limit')
-            .and.to.eql([
-                { type: 'number', value: 0 },
-                { type: 'number', value: 5 }
-            ]);
-    });
+        it('should apply page to lateral join', () => {
+            const ast = queryBuilder({
+                attributes: ['col2'],
+                queryAst,
+                limitPer: 'col1',
+                filter: [[{ attribute: 'col1', operator: 'equal', valueFromParentKey: true, value: [1, 3] }]],
+                order: [{ attribute: 'col2', direction: 'desc' }],
+                limit: 10,
+                page: 5
+            });
 
-    it('should apply page to lateral join', () => {
-        const ast = queryBuilder({
-            attributes: ['col2'],
-            queryAst,
-            limitPer: 'col1',
-            filter: [[{ attribute: 'col1', operator: 'equal', valueFromParentKey: true, value: [1, 3] }]],
-            order: [{ attribute: 'col2', direction: 'desc' }],
-            limit: 10,
-            page: 5
+            const [, { expr: subSelect }] = ast.from;
+            expect(subSelect)
+                .to.have.property('limit')
+                .and.to.eql([
+                    { type: 'number', value: 40 },
+                    { type: 'number', value: 10 }
+                ]);
         });
-
-        const [, { expr: subSelect }] = ast.from;
-        expect(subSelect)
-            .to.have.property('limit')
-            .and.to.eql([
-                { type: 'number', value: 40 },
-                { type: 'number', value: 10 }
-            ]);
     });
 
     it('should apply order to lateral join', () => {
@@ -302,5 +306,5 @@ describe('query-builder (limit-per)', () => {
             .and.to.eql([{ expr: { type: 'column_ref', table: 't', column: 'col2' }, type: 'DESC' }]);
     });
 
-    xit('should optimize lateraled/original query'); // remove unused columns and joins
+    xit('should optimize sub-query'); // remove unused columns and joins
 });
